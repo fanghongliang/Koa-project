1cbcd986cc76108ef2dfa7a99ab5b6234c3ba6a8
fanghongliang <18292656094@163.com>
2019-08-22 20:32:50 +0800

    feat:'获取HTTP传输的四种参数'

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,10 +1,23 @@
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/v1/classics/latest', (ctx, next)=> {
-  ctx.body = {
-    key: "classics",
-    name: "fanghl"
+router.post('/v1/:id/classic/latest', (ctx, next)=> {
+  const path = ctx.params    //url链接中参数
+  const query = ctx.request.query     //?后面的query参数
+  const headers = ctx.request.header    //header里面的参数，如token
+  const body = ctx.request.body   //body里面的json参数
+  if(path.id == 3 ) {
+    ctx.body = {
+      key: "classics",
+      name: "fanghl",
+      content: `你成功传输了前后端内容${path.id}${body.name}`
+    } 
+  } else {
+    ctx.body = {
+      key: "classics",
+      name: "fanghl",
+      content: `你fail传输了前后端内容`
+    }
   }
 })
 
