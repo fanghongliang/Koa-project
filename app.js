7dc44b578f77c53c86e827ca1fc3f1bc9505c462
fanghongliang <18292656094@163.com>
2019-08-22 17:55:20 +0800

    refactor:'重构自动注册路由'

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const Koa = require('koa')
+const requireDirectory = require('require-directory')
 const book = require('./api/v1/book')
 const classic = require('./api/v1/classic')
+const Router = require('koa-router')
 
 const app = new Koa()
 
-app.use(book.routes())
-app.use(classic.routes())
+const modules = requireDirectory(module, './api', {
+  visit: whenLoadModules
+})
+
+function whenLoadModules(obj) {
+  if( obj instanceof Router ) {
+    app.use(obj.routes())
+  }
+}
+// app.use(book.routes())    添加自动注册插件 requireDirectory 
+// app.use(classic.routes())
 
 app.listen(3000)
